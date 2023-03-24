933fcc6be0a72b845c3af99d8a0d2dc5d409318b
Bibhanshu <bibhanshuya@gmail.com>
2023-03-24 23:03:52 +0530

    Simple Final Commit

--- app.js
+++ app.js
@@ -17,6 +17,18 @@ const addToDo = (item) => {
         ${item}
     <i class="fas fa-times"></i>
     `;
+    listItem.addEventListener(
+        "click",
+        function(){
+            this.classList.toggle("done");
+        }
+    )
 
+    listItem.querySelector("i").addEventListener(
+        "click",
+        function(){
+            listItem.remove();
+        }
+    )
     toDoBox.appendChild(listItem)
 }
